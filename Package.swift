a21b180be6b0ede635c1073c59a40144118ba3cd
Tony <54366703+tonylepage@users.noreply.github.com>
2023-11-15 17:35:50 +0800

    update binaries to swift v5.8

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "MLYSDK",
     platforms: [
-        .iOS(.v15),
-        .tvOS(.v15),
+        .iOS(.v13),
+        .tvOS(.v13),
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.

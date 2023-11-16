1b95a0d649b0fa5933a9676a2af519c43fca35e8
Tony <54366703+tonylepage@users.noreply.github.com>
2023-11-16 19:47:07 +0800

    fix for swift 5.8.1

--- Package.swift
+++ Package.swift
@@ -13,22 +13,16 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "MLYSDK",
-            targets: ["MLYSDK", "MUXSDKStats"]),
+            targets: ["MLYSDK"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/tonylepage/GCDWebServer", .upToNextMajor(from: "3.5.6")),
-        .package(url: "https://github.com/getsentry/sentry-cocoa", .upToNextMajor(from: "7.31.3")),
-        .package(url: "https://github.com/webrtc-sdk/Specs", .upToNextMajor(from: "104.5112.16")),
-        .package(url: "https://github.com/centrifugal/centrifuge-swift", .upToNextMajor(from: "0.5.5")),
-        .package(url: "https://github.com/apple/swift-protobuf", .upToNextMajor(from: "1.21.0"))
+        .package(url: "https://github.com/yene/GCDWebServer", .upToNextMajor(from: "3.5.4")),
+        .package(url: "https://github.com/muxinc/mux-stats-sdk-avplayer.git", .upToNextMajor(from: "3.1.0")),
+        .package(url: "https://github.com/getsentry/sentry-cocoa", .upToNextMajor(from: "7.31.3"))
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .binaryTarget(
-            name: "MUXSDKStats",
-            path: "BinaryFrameworks/MUXSDKStats.xcframework"
-        ),
         .binaryTarget(
             name: "MLYSDK",
             path: "BinaryFrameworks/MLYSDK.xcframework"
